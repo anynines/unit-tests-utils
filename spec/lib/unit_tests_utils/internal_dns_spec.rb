08b4ba96d98394cfedcdecb77cf6c281e99acdbc
Lucas 'medk' Pinto <lucaspinto33@gmail.com>
2017-08-04 13:07:36 +0200

    Add more tests

--- spec/lib/unit_tests_utils/internal_dns_spec.rb
+++ spec/lib/unit_tests_utils/internal_dns_spec.rb
@@ -47,8 +47,53 @@ describe UnitTestsUtils::InternalDNS do
     end
   end
 
-  describe "valid_hostnames?"
-  describe "valid_ips?"
+  describe ".valid_hostnames?" do
+    let(:hostname1) { "hostname1.domain" }
+    let(:hostname2) { "hostname2.domain" }
+    before :each do
+      allow(UnitTestsUtils::InternalDNS).
+        to receive(:resolv).with(hostname1).and_return("1.2.3.4")
+    end
+
+    context "when the list is invalid" do
+      before :each do
+        allow(UnitTestsUtils::InternalDNS).
+          to receive(:resolv).with(hostname2).and_return("no")
+      end
+
+      it "returns false" do
+        expect(UnitTestsUtils::InternalDNS.valid_hostnames?([hostname1, hostname2])).
+          to be_falsey
+      end
+    end
+
+    context "when the list is valid" do
+      before :each do
+        allow(UnitTestsUtils::InternalDNS).
+          to receive(:resolv).with(hostname2).and_return("2.3.4.5")
+      end
+            it "returns true" do
+        expect(UnitTestsUtils::InternalDNS.valid_hostnames?([hostname1, hostname2])).
+          to be_truthy
+      end
+    end
+  end
+
+  describe ".valid_ips?" do
+    context " when a list is invalid" do
+      let(:invalid_list) { ["1.2.3.4", "no"] }
+      it "return false" do
+        expect(UnitTestsUtils::InternalDNS.valid_ips?(invalid_list)).to be_falsey
+      end
+    end
+
+    context "when a list is valid" do
+      let(:valid_list) { ["1.2.3.4", "111.222.333.444"] }
+      it "return true" do
+        expect(UnitTestsUtils::InternalDNS.valid_ips?(valid_list)).to be_truthy
+      end
+    end
+  end
 end
 
 
