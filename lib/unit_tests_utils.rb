94ba405a1ebf90f6188cd73e9746d4945060c9b1
Kevin Konrad <kkonrad@avarteq.de>
2019-03-12 16:15:23 +0100

    brings turbulence to the unit tests utils

--- lib/unit_tests_utils.rb
+++ lib/unit_tests_utils.rb
@@ -1,8 +1,9 @@
 module UnitTestsUtils
   autoload :Bosh, 'unit_tests_utils/bosh.rb'
   autoload :Consul, 'unit_tests_utils/consul.rb'
+  autoload :Git, 'unit_tests_utils/git.rb'
   autoload :InternalDNS, 'unit_tests_utils/internal_dns.rb'
   autoload :Manifest, 'unit_tests_utils/manifest.rb'
-  autoload :Git, 'unit_tests_utils/git.rb'
   autoload :PGWebServiceClient, 'unit_tests_utils/postgresql_web_service_client.rb'
+  autoload :Turbulence, 'unit_tests_utils/turbulence.rb'
 end
