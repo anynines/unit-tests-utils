0e73eb3c1ccd16dd94556f861b0e5cbf6ed9acdb
fgermano <fgermano@anynines.com>
2021-02-14 16:14:03 -0300

    [WIP] Full PR

--- lib/unit_tests_utils/postgresql_client.rb
+++ lib/unit_tests_utils/postgresql_client.rb
@@ -9,19 +9,20 @@ class UnitTestsUtils::PostgreSQLClient
 
   def initialize(args)
     @args = args
-    @args[:dbname] ||= 'area51'
+    @args[:dbname] ||= 'admin'
     @args[:sslmode] ||= 'disable'
   end
 
-  def self.create_from_manifest(manifest, host = nil)
-    host = manifest.hostname unless host
-    user = manifest.properties['postgresql-ha']['admin_credentials']['username']
-    password = manifest.properties['postgresql-ha']['admin_credentials']['password']
+  def self.create_from_manifest(manifest, args = {})
+    args[:host] ||= manifest.hostname
+    args[:user] = manifest.properties['postgresql-ha']['admin_credentials']['username']
+    args[:password] = manifest.properties['postgresql-ha']['admin_credentials']['password']
+
     if manifest.properties("#{POSTGRESQL_PROPERTIES_PATH}/postgresql-ha/ssl?/enable")
-      sslmode = "require"
+      args[:sslmode] = "require"
     end
 
-    self.new(host: host, user: user, password: password, sslmode: sslmode)
+    self.new(args)
   end
 
   def ping
@@ -61,5 +62,17 @@ class UnitTestsUtils::PostgreSQLClient
     return conn.exec(sql)
   ensure
     conn.close if !conn.nil?
+  end
+
+  private
+
+  def connect(extra_args = {})
+    args.merge!(extra_args)
+    logger.debug("* Creating connection with args: *#{args}*")
+    return PG::Connection.new(args)
+  end
+
+  def test_table
+    'a9s_pg_tests'
   end
 end
